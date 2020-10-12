aee7ca87f51987761bdf7459bec202031bc25dc6
Jordan <Jordan.cornelis@gmail.com>
2020-10-12 12:05:20 +0200

    Added PlayerCombat script

---
+++ PM-MakingAGame/Assets/PlayerCombat.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCombat : MonoBehaviour
+{
+    public Animator animator;
+
+    public Transform attackPoint;
+    public LayerMask EnemyLayers;
+
+    public float AttackRange = 0.5f;
+    public int AttackDamage = 30;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Attack();
+        }
+    }
+    void Attack()
+    {
+        animator.SetTrigger("Attack");
+
+        Collider2D[] HitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, AttackRange, EnemyLayers);
+
+        foreach (var enemy in HitEnemies)
+        {
+            Debug.Log("We hit" + enemy.name);
+            //enemy.GetComponent<Enemy>().TakeDamage(AttackDamage);
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+        {
+            return;
+        }
+        Gizmos.DrawWireSphere(attackPoint.position, AttackRange);
+    }
+
+}
