6d269ede455c1c365e75ae763ce57ced39bca79e
Casper De Keyser <casper.dekeyser@student.ap.be>
2020-10-15 21:25:16 +0200

    Programming pathfinding
    Basic initial pathfinding, no update

--- PM-MakingAGame/Assets/EnemyAI.cs
+++ PM-MakingAGame/Assets/EnemyAI.cs
@@ -1,14 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
+using Pathfinding;
+using System;
 
 public class EnemyAI : MonoBehaviour
 {
+
+    public Transform target;
+    public float speed = 200f;
+    public float nextWaypointDistance = 3f;
+
+    private Path path;
+    private int currentWaypoint = 0;
+    private bool reachedEndOfPath = false;
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        seeker.StartPath(rb.position, target.position, OnPathComplete);
     }
+
+    private void OnPathComplete(Path p)
+    {
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+
 
     // Update is called once per frame
     void Update()
