e806016d90cf0e507199cc2e8088ad989a6bde96
Jordan <Jordan.cornelis@gmail.com>
2020-10-16 11:11:27 +0200

    EnemyCombatscript added

---
+++ PM-MakingAGame/Assets/EnemyCombat.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCombat : MonoBehaviour
+{
+    public int MaxHealth = 100;
+    private int currentHealth;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = MaxHealth;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        Debug.Log("Enemy died!");
+    }
+}
